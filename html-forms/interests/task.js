f4a290e36be8a9521b2e73ae1857ea5f803e6f22
Nikita Dedkov <dedkovnd@gmail.com>
2020-03-25 21:05:21 +0300

    first commit

---
+++ html-forms/interests/task.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".interest__check").forEach( elem=> {
+    if(elem.checked) {
+        console.log("qwertyu")//не получилось даже в консоль вывести что-то для теста
+    }
+})
