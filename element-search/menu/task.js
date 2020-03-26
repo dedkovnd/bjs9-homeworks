2415b8c4abba27331905f5fc8af5677f0a86aa96
Nikita Dedkov <dedkovnd@gmail.com>
2020-03-26 21:16:25 +0300

    first commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,12 +1,30 @@
-let links = Array.from(document.getElementsByClassName("menu__link"));
+let links = Array.from(document.querySelectorAll(".menu_main > .menu__item > .menu__link"));
 
 
 links.forEach(elem=> {
-    elem.addEventListener("click", e=> {
-        elem.closest(".menu__item").classList.add("menu_active");
-        return false;
-    })
+    elem.addEventListener("click", linkHandler)
 })
+
+function linkHandler(e) {
+    let parent = e.currentTarget.closest('.menu__item')
+    if(parent.childElementCount > 1) {
+        setActive(e.currentTarget)
+        e.preventDefault()
+    }
+}
+
+function setActive(elem) {
+    let menu_sub = elem.nextElementSibling
+    if(menu_sub.classList.contains('menu_active')) {
+        menu_sub.classList.remove('menu_active')
+    } else {
+        let active = document.querySelector('.menu_active')
+        active !== null ? active.classList.remove('menu_active') : null
+        menu_sub.classList.add('menu_active')
+    }
+
+
+}
 
 
 
