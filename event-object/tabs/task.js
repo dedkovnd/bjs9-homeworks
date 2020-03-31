82f1e1f7e933a24e7cf5396089466940293f8f36
Nikita Dedkov <dedkovnd@gmail.com>
2020-03-31 14:57:46 +0300

    this working code

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -6,22 +6,20 @@ tabs.forEach( elem=> {
         tabs.forEach(elem=>{
             elem.classList.remove("tab_active")
         })
+        content.forEach(elem=>{
+            elem.classList.remove("tab__content_active")
+        })
+
         if ((tabs.indexOf(elem) === 0)) {
             elem.classList.add("tab_active")
             content[0].classList.add("tab__content_active")
-            content[1].classList.remove("tab__content_active")
-            content[2].classList.remove("tab__content_active")
         }
         else if(tabs.indexOf(elem) === 1) {
             elem.classList.add("tab_active")
             content[1].classList.add("tab__content_active")
-            content[0].classList.remove("tab__content_active")
-            content[2].classList.remove("tab__content_active")
         } else if (tabs.indexOf(elem) === 2) {
             elem.classList.add("tab_active")
             content[2].classList.add("tab__content_active")
-            content[0].classList.remove("tab__content_active")
-            content[1].classList.remove("tab__content_active")
         }
     })
 })
