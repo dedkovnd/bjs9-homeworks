edf758a033de3a41f05a9a18775694ec99b6386a
Nikita Dedkov <dedkovnd@gmail.com>
2020-03-31 14:47:28 +0300

    add workong code

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,9 +1,27 @@
- document.querySelectorAll(".tab").forEach(elem=> {
-     elem.addEventListener("click", e=> {
-         if(!elem.classList.contains(".tab_active")) {
-             //веронятно здесь могло бы быть решение через toggle, но если контент и вкладки не привязаны друг к другу,
-             //то какая должна быть логика?
-         }
-     })
- })
+let tabs = Array.from(document.querySelectorAll(".tab"));
+let content = Array.from(document.querySelectorAll(".tab__content"));
 
+tabs.forEach( elem=> {
+    elem.addEventListener("click", e=> {
+        tabs.forEach(elem=>{
+            elem.classList.remove("tab_active")
+        })
+        if ((tabs.indexOf(elem) === 0)) {
+            elem.classList.add("tab_active")
+            content[0].classList.add("tab__content_active")
+            content[1].classList.remove("tab__content_active")
+            content[2].classList.remove("tab__content_active")
+        }
+        else if(tabs.indexOf(elem) === 1) {
+            elem.classList.add("tab_active")
+            content[1].classList.add("tab__content_active")
+            content[0].classList.remove("tab__content_active")
+            content[2].classList.remove("tab__content_active")
+        } else if (tabs.indexOf(elem) === 2) {
+            elem.classList.add("tab_active")
+            content[2].classList.add("tab__content_active")
+            content[0].classList.remove("tab__content_active")
+            content[1].classList.remove("tab__content_active")
+        }
+    })
+})
