936eea65e9f6ad61e9697ad6fc3bd4e7cced1cee
Nikita Dedkov <dedkovnd@gmail.com>
2020-04-17 20:19:19 +0300

    fixed function getRotator

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,18 +1,19 @@
 let rtcase = Array.from(document.getElementsByClassName("rotator__case"));
+let count = 0;
 
-function getRotation() {
-    for (let i = 0; i < rtcase.length; i++) {
-        if (rtcase[i].classList.contains("rotator__case_active")) {
-            rtcase[i].classList.remove("rotator__case_active")
-            rtcase[i + 1].classList.add("rotator__case_active")
-            if (i === rtcase.length - 2) {
-                rtcase[rtcase.length - 1].classList.remove("rotator__case_active")
-                rtcase[0].classList.add("rotator__case_active")
-                i = 0;
-            }
-        }
-
-    }}
+function getRotation () {
+    let caseElem = document.querySelector(".rotator__case_active")
+    count++;
+    console.log(rtcase.length)
+    if (count !== rtcase.length) {
+        caseElem.classList.remove("rotator__case_active")
+        caseElem.nextElementSibling.classList.add("rotator__case_active")
+    } else {
+        rtcase[rtcase.length - 1].classList.remove("rotator__case_active")
+        rtcase[0].classList.add("rotator__case_active")
+        count = 0;
+    }
+}
 
 setInterval(getRotation, 1000)
 
