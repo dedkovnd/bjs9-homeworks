e3c00f2a657dc41f67dd9c126bd721de1abab03f
Nikita Dedkov <dedkovnd@gmail.com>
2020-04-17 20:38:01 +0300

    add var formData, but event.total const 0

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,23 +1,19 @@
-// document.getElementById("form").addEventListener("submit", e=>{
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php")
-//     xhr.addEventListener("readystatechange", e=> {
-//         let progress = document.getElementById("progress");
-//         console.log(progress.value)
-//     })
-// })
+document.getElementById("form").addEventListener("submit", e=> {
+    event.preventDefault();
+    let formData = new FormData(document.forms[0]);
+    let xhr = new XMLHttpRequest();
+    let progress = document.getElementById("progress");
+    xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php")
+    xhr.send(formData);
+    xhr.onprogress = () => {
+        progress.value = event.loaded/event.total;
+        console.log(progress.value)
+        console.log(event.total)
+        console.log(event.loaded)
+    }
+})
 
-//Подскажите, на что я не обратил внимание. На специфицеское поведение формы с атрибутом action или на более базовую вещь?
-//Возможно надо было "слушать" не submit, а другое событие? И с чем правильней работать, с readystatechange или с upload.onprogress?
 
-// document.getElementById("form").addEventListener("submit", e=> {
-//     let xhr = new XMLHttpRequest();
-//     let progress = document.getElementById("progress");
-//     xhr.open("POST", "https://netology-slow-rest.herokuapp.com/upload.php")
-//     xhr.upload.onprogress = () => {
-//         console.log(progress.value)
-//     }
-// })
 
 
 
