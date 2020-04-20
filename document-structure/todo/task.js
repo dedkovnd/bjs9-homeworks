275bb731842ddc3558e90a98a8375310ec7c50b1
Nikita Dedkov <dedkovnd@gmail.com>
2020-04-20 18:59:33 +0300

    fixed problem with lenght taskMessage

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,24 +1,30 @@
 let task = document.getElementById("task__input")
 let taskList = document.getElementById("tasks__list")
 
-
-    document.addEventListener("keydown", (event)=> {
+document.addEventListener("keydown", (event)=> {
         if (event.keyCode === 13) {
-        let taskMessage = `<div class="task">
-    <div class="task__title">
-      ${task.value}
-    </div>
-    <a href="#" class="task__remove">&times;</a>
-    </div>`
-        taskList.insertAdjacentHTML("afterbegin", taskMessage)
-        task.value = "";
-        let taskRemove = Array.from(document.getElementsByClassName("task__remove"))
-        taskRemove.forEach(elem => {
-                elem.addEventListener("click",()=>{
-                    elem.parentElement.remove()
-                })
+            let taskMessage = `<div class="task">
+            <div class="task__title">
+            ${task.value}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+            </div>`
+
+            if (task.value.length > 0) {
+                taskList.insertAdjacentHTML("afterbegin", taskMessage)
+                task.value = "";
+            }
+            let taskRemove = Array.from(document.getElementsByClassName("task__remove"));
+            taskRemove[0].addEventListener("click", ()=> {
+                taskRemove[0].parentElement.remove()
             })
         event.preventDefault()
     }
 })
+
+
+
+
+
+
 
