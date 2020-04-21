375c8aada83f75b1be7760ed31065b0dbeac6623
Nikita Dedkov <dedkovnd@gmail.com>
2020-04-21 12:59:44 +0300

    delete bicycle creative

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,21 +1,19 @@
 let rtcase = Array.from(document.getElementsByClassName("rotator__case"));
-let count = 0;
 
 function getRotation () {
     let caseElem = document.querySelector(".rotator__case_active")
-    count++;
-    console.log(rtcase.length)
-    if (count !== rtcase.length) {
+    if (caseElem.nextElementSibling !== null) {
         caseElem.classList.remove("rotator__case_active")
         caseElem.nextElementSibling.classList.add("rotator__case_active")
     } else {
         rtcase[rtcase.length - 1].classList.remove("rotator__case_active")
         rtcase[0].classList.add("rotator__case_active")
-        count = 0;
     }
 }
 
 setInterval(getRotation, 1000)
+
+
 
 
 
