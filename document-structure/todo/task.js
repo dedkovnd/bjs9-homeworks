8d2dcf1dc8afa1bf703ae12593387b950560cb5d
Nikita Dedkov <dedkovnd@gmail.com>
2020-04-22 15:23:09 +0300

    add listener on button

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,26 +1,36 @@
 let task = document.getElementById("task__input")
 let taskList = document.getElementById("tasks__list")
 
-document.addEventListener("keydown", (event)=> {
-        if (event.keyCode === 13) {
-            let taskMessage = `<div class="task">
+function addTask () {
+    let taskMessage = `<div class="task">
             <div class="task__title">
             ${task.value}
             </div>
             <a href="#" class="task__remove">&times;</a>
             </div>`
 
-            if (task.value.length > 0) {
-                taskList.insertAdjacentHTML("afterbegin", taskMessage)
-                task.value = "";
-            }
-            let taskRemove = Array.from(document.getElementsByClassName("task__remove"));
-            taskRemove[0].addEventListener("click", ()=> {
-                taskRemove[0].parentElement.remove()
-            })
-        event.preventDefault()
+    if (task.value.length > 0) {
+        taskList.insertAdjacentHTML("afterbegin", taskMessage)
+        task.value = "";
     }
+    let taskRemove = Array.from(document.getElementsByClassName("task__remove"));
+    taskRemove[0].addEventListener("click", ()=> {
+        taskRemove[0].parentElement.remove()
+    })
+}
+
+document.addEventListener("keydown", (event)=> {
+        if (event.keyCode === 13) {
+            addTask();
+            event.preventDefault()
+        }
 })
+
+document.getElementById("tasks__add").addEventListener("click", ()=> {
+    addTask();
+    event.preventDefault()
+})
+
 
 
 
